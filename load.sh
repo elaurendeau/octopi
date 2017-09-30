1b54ecc43b8778e1535e5e3ceb21145d8a874f85
Elliot Laurendeau <laurendeau.elliot@gmail.com>
2017-09-29 21:19:27 -0400

    added proper path

--- load.sh
+++ load.sh
@@ -6,13 +6,13 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git /home/pi/bash_it
 /home/pi/bash_it/install.sh
 bash-it enable plugin git
 cd /home/pi/octopi
-sudo chmod 777 /home/pi/haproxy.cfg
-sudo chmod 777 /home/pi/smb.conf
+sudo chmod 777 /home/pi/octopi/haproxy.cfg
+sudo chmod 777 /home/pi/octopi/smb.conf
 sudo mv /etc/samba/smb.conf /etc/samba/smb.old
-sudo cp /home/pi/smb.conf /etc/samba/smb.conf
+sudo cp /home/pi/octopi/smb.conf /etc/samba/smb.conf
 sudo service smbd restart
 sudo smbpasswd -a pi
 sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.old
-sudo cp /home/pi/haproxy.cfg /etc/haproxy/haproxy.cfg
+sudo cp /home/pi/octopi/haproxy.cfg /etc/haproxy/haproxy.cfg
 sudo -E vim /etc/haproxy/haproxy.cfg
 sudo reboot now
