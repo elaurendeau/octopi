6f905bea4427570bf4fd632f2997ccdd2b6f9fc4
Elliot Laurendeau <laurendeau.elliot@gmail.com>
2017-09-29 21:13:30 -0400

    fixed home path

--- load.sh
+++ load.sh
@@ -1,18 +1,18 @@
-cd ~
+cd /home/pi
 sudo apt-get install -y vim
 git clone https://github.com/elaurendeau/dotfiles.git
-cp ~/dotfiles/_vimrc ~
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+cp /home/pi/dotfiles/_vimrc /home/pi/
+git clone --depth=1 https://github.com/Bash-it/bash-it.git /home/pi/bash_it
+/home/pi/bash_it/install.sh
 bash-it enable plugin git
-cd ~/octopi
-sudo chmod 777 haproxy.cfg
-sudo chmod 777 smb.conf
+cd /home/pi/octopi
+sudo chmod 777 /home/pi/haproxy.cfg
+sudo chmod 777 /home/pi/smb.conf
 mv /etc/samba/smb.conf /etc/samba/smb.old
-cp smb.conf /etc/samba/smb.conf
+cp /home/pi/smb.conf /etc/samba/smb.conf
 sudo service smbd restart
 sudo smbpasswd -a pi
 mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.old
-cp haproxy.cfg /etc/haproxy/haproxy.cfg
+cp /home/pi/haproxy.cfg /etc/haproxy/haproxy.cfg
 sudo -E vim /etc/haproxy/haproxy.cfg
 sudo reboot now
