c55d3ac9aed64fe51031cc596491dafc70e3d2e6
Elliot <tapoox@gmail.com>
2017-10-01 04:29:21 +0000

    added logs to the restart

--- wifi_rebooter.sh
+++ wifi_rebooter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #The IP for the server you wish to ping (8.8.8.8 is a public Google DNS server)
-SERVER=8.8.8.8
+SERVER=192.168.0.1
 
 #Only send two pings, sending output to /dev/null
 ping -c2 $SERVER > /dev/null
@@ -9,7 +9,9 @@ ping -c2 $SERVER > /dev/null
 # If the return code from ping ($?) is not 0 (meaning there was an error)
 if [ $? != 0 ]
 then
+	echo "$(date): No network connection, restarting wlan0" >> /var/log/wifi_restart.log
 	# Restart the wireless interface
 	ifdown --force wlan0
+	sleep 5
 	ifup wlan0
 fi
